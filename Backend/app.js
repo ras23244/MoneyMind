03759b341a8aed75b8f77e82cedce9b007df9124
ras23244 <146091681+ras23244@users.noreply.github.com>
2026-02-15 17:46:05 +0530

    Set trust proxy for Express app

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
+
 const express = require('express');
 const app = express();
-
+app.set('trust proxy', 1);
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
